ae3793d8ae98b3cb9e34050589ad1f1ca27242c4
Aienz Lugue <aienzlugue929@gmail.com>
2025-02-10 00:36:56 +0800

    deleted animations, added grid movement
    i removed the animations + the one node with the two smaller node for idle and walk
    
    also can we change walk_front and walk_back to walk_up and walk_down

--- scenes/characters/walk_state.gd
+++ scenes/characters/walk_state.gd
@@ -2,39 +2,47 @@ extends NodeState
 
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
-@export var speed: int = 150
 
 func _on_process(_delta : float) -> void:
 	pass
 
-
 func _on_physics_process(_delta : float) -> void:
-	var direction: Vector2 = GameInputEvents.movement_input()
-	
-	if direction == Vector2.UP:
-		animated_sprite_2d.play("walk_back")
-	elif direction == Vector2.DOWN:
+	self.input_dir = Vector2.ZERO
+	if Input.is_action_just_pressed("ui_down"):
 		animated_sprite_2d.play("walk_front")
-	elif direction == Vector2.RIGHT:
+		self.player_direction = Vector2(0,1)
+		move()
+	elif Input.is_action_just_pressed("ui_up"):
+		animated_sprite_2d.play("walk_back")
+		self.player_direction = Vector2(0,-1)
+		move()
+	elif Input.is_action_just_pressed("ui_right"):
 		animated_sprite_2d.play("walk_right")
-	elif direction == Vector2.LEFT:
+		self.player_direction = Vector2(1,0)
+		move()
+	elif Input.is_action_just_pressed("ui_left"):
 		animated_sprite_2d.play("walk_left")
-	
-	if direction != Vector2.ZERO:
-		player.player_direction = direction
-	
-	player.velocity = direction * speed
-	player.move_and_slide()
+		self.player_direction = Vector2(-1,0)
+		move()
 
+func move():
+	if self.player_direction: 
+		if self.moving == false:
+			self.moving = true
+			var tween = create_tween()
+			tween.tween_property(self, "position", self.position + self.player_direction * self.tile_size, 0.35)
+			tween.tween_callback(move_false)
+
+func move_false():
+	transition.emit("Idle")
+	self.moving = false
 
 func _on_next_transitions() -> void:
 	if !GameInputEvents.is_movement_input():
 		transition.emit("Idle")
 
-
 func _on_enter() -> void:
 	pass
-
 
 func _on_exit() -> void:
 	animated_sprite_2d.stop()
